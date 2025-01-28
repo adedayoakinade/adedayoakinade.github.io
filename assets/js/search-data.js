0fe4e2a0eaf27f6e9e5fc8935f7926f031c44603
adedayoakinade <43846225+adedayoakinade@users.noreply.github.com>
2025-01-28 21:32:20 +0000

    Deploying to gh-pages from @ adedayoakinade/adedayoakinade.github.io@35abff4d93be541eda2a9f06cdb1fd14fef668d7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,7 +492,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%64%65%64%61%79%6F.%61%6B%69%6E%61%64%65@%6D%6F%6E%61%73%68.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -513,7 +513,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=drFv8acAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
